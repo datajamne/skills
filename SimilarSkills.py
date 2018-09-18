3c0ceceae52d1884c85c2c97c4515344bbadae88
abobrowska <dwp.ds.ncl.akb@gmail.com>
2018-09-18 14:58:37 +0100

    Fix: Fixed job title recommendation

--- SimilarSkills.py
+++ SimilarSkills.py
@@ -27,7 +27,7 @@ B=Adj.divide(Diag)
 Corr=B.multiply(B.transpose())
 
 
-# In[45]:
+# In[98]:
 
 
 # Choosing n skills most correlated to user's skills (mySkills)
@@ -35,6 +35,7 @@ def SimilarSkills(mySkills, n) :
     m=len(mySkills)
     Similar=Corr.loc[mySkills].sum()
     Similar=Similar.sort_values(ascending=False)[m:]
+    Similar=Similar.index.values
     return Similar[:n]
 # Test
 #mySkills = ['.Net', 'Sales','Marketing','Business Development']
@@ -51,13 +52,13 @@ SkillSalary=pd.read_csv('https://datajam2018.blob.core.windows.net/eventopendata
 Salary=SkillSalary.loc[201807]
 
 
-# In[51]:
+# In[99]:
 
 
 # For each recommended skill return median salary
 def SimilarSalary (mySkills, n) :
     mySimilarSkills=SimilarSkills(mySkills, 10)
-    mySimilarSalary=Salary[mySimilarSkills.index.values]
+    mySimilarSalary=Salary[mySimilarSkills]
     return mySimilarSalary
 
 # Test
@@ -76,27 +77,38 @@ SkillJobs=pd.read_csv('https://datajam2018.blob.core.windows.net/eventopendata/n
 SkillJobs=SkillJobs.set_index('normalised_job_title')
 
 
-# In[82]:
+# In[116]:
 
 
 # For a list of skills return related job titles
-JobFreq=SkillJobs.sum(axis=1) # Get total frequency for all jobs
-RelativeFreq=SkillJobs.div(JobFreq, axis=0) # For each job get relative frequency of skills
+JobFreq=SkillJobs.sum(axis=0) # Get total frequency for all skills
+RelativeFreq=SkillJobs.div(JobFreq, axis=1) # For each skill get raltive frequency of jobs
 
 def RelatedJobs (mySkills, n) :
     MyRelativeFreq=RelativeFreq[mySkills] # Pick out relative frequency of users skills
     myJobs=MyRelativeFreq.sum(axis=1)
-    myJobs=myJobs.sort_values(ascending=False)
+    myJobs=myJobs.sort_values(ascending=False).index.values
     return myJobs[:n]
 
+def CurrentJobs (mySkills,n) :
+    mySkills_np=np.array(mySkills)
+    return RelatedJobs(mySkills_np,n)
 
-# In[93]:
+def RecommendedJobs (mySkills, n, m) :
+    mySkills_np=np.array(mySkills)
+    allSkills=np.append(mySkills_np,SimilarSkills(mySkills,n))
+    return RelatedJobs(allSkills,m)
 
 
-#mySkills = ['Sales','Marketing','Business Development','Project Management']
+# In[117]:
+
+
+mySkills = ['Sales','Marketing','Business Development','Project Management']
 #mySkills =['Analysis', 'Statistics','Data','Microsoft Excel', 'R']
-#Sims=SimilarSkills(mySkills,10).index.values
+#Sims=SimilarSkills(mySkills,10)
 #AllSkills=np.append(np.array(mySkills),Sims)
 #RelatedJobs(mySkills.append(SimilarSkills(mySkills,10)),10)
-#RelatedJobs(AllSkills,10)
+print(Sims)
+print(CurrentJobs(mySkills,10))
+print(RecommendedJobs(mySkills,10,10))
 
